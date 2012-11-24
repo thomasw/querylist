6dd5b9771a986a72386ba12a5b390e2dacf8c811
Thomas W <thomas@matchstrike.net>
2012-11-23 17:00:15 -0800

    Reconfigure testtube so nosetests are run first
    The test suite output was too long and that was causing pep8 and pyflakes output to be pushed above the fold.

--- tube.py
+++ tube.py
@@ -1,5 +1,5 @@
 from testtube.helpers import pep8_all, pyflakes_all, nosetests_all
 
 PATTERNS = (
-    (r'.*\.py', [pep8_all, pyflakes_all, nosetests_all]),
+    (r'.*\.py', [nosetests_all, pep8_all, pyflakes_all]),
 )
