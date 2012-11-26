f807bd489b828369b69caed33583f276d527ce45
Thomas W <thomas@matchstrike.net>
2012-11-25 18:13:41 -0800

    Configure tube to rebuild the docs on file changes.

--- tube.py
+++ tube.py
@@ -1,5 +1,13 @@
+import subprocess
 from testtube.helpers import pep8_all, pyflakes_all, nosetests_all
 
+
+def make_docs(changed, **kwargs):
+    print 'Regenerating documentation...\n'
+    subprocess.call(['make', '-C', 'docs/', 'html'])
+
+
 PATTERNS = (
-    (r'.*\.py', [nosetests_all, pep8_all, pyflakes_all]),
+    (r'.*\.py', [nosetests_all, pep8_all, pyflakes_all, make_docs]),
+    (r'(?P<path>.*)\.rst', [make_docs]),
 )
