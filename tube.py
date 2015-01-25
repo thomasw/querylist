1bab12e23e3ad0089312b15f7a4b131efeb36468
Thomas W <thomas@matchstrike.net>
2015-01-24 21:40:28 -0800

    Upgrade testtube to the latest version
    This also adds frosted, flake8, and pep257 linting to the testtube
    config.

--- tube.py
+++ tube.py
@@ -1,13 +1,18 @@
-import subprocess
-from testtube.helpers import pep8_all, pyflakes_all, nosetests_all
+from testtube.helpers import Flake8, Frosted, Helper, Nosetests, Pep257
 
 
-def make_docs(changed, **kwargs):
-    print 'Regenerating documentation...\n'
-    subprocess.call(['make', '-C', 'docs/', 'html'])
+class MakeDocs(Helper):
+    command = 'make'
+    all_files = True
+
+    def get_args(self):
+        return ['-C', 'docs/', 'html']
 
 
 PATTERNS = (
-    (r'.*\.py', [nosetests_all, pep8_all, pyflakes_all, make_docs]),
-    (r'(?P<path>.*)\.rst', [make_docs]),
+    (r'((?!test_)(?!tube\.py).)*\.py$', [Pep257(bells=0)]),
+    (r'.*\.py$', [Flake8(all_files=True), Frosted(all_files=True)],
+     {'fail_fast': True}),
+    (r'(.*setup\.cfg$)|(.*\.coveragerc)|(.*\.py$)', [Nosetests()]),
+    (r'.*\.rst$', [MakeDocs()])
 )
