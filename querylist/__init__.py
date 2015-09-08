d6726447559b371886ae9885702a5da71c25c0fe
Thomas W <thomas@matchstrike.net>
2015-09-07 23:49:20 -0700

    Fix single vs double quote inconsistency

--- querylist/__init__.py
+++ querylist/__init__.py
@@ -2,7 +2,7 @@
 from querylist.list import QueryList
 from querylist.dict import BetterDict
 
-__version__ = "0.2.0"
-__author__ = "Thomas Welfley"
+__version__ = '0.2.0'
+__author__ = 'Thomas Welfley'
 
 __all__ = ['QueryList', 'BetterDict']
